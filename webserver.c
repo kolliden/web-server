e70eb83de2f5abc90ba9ff00c01abaa82c8f851f
konstantin <konstantinmuehlberg@gmail.com>
2025-11-11 15:24:50 +0100

    feat(statischer Inhalt, Status Codes)

--- webserver.c
+++ webserver.c
@@ -14,6 +14,33 @@
 #define BACKLOG 10      // how many pending connections queue holds
 #define MAXDATASIZE 100 // max number of bytes we can get at once
 
+enum Method
+{
+    GET = 1,
+    POST = 2,
+    PUT = 3,
+    DELETE = 4,
+    UNKNOWN = 5,
+    BADREQUEST = 0
+};
+
+typedef struct Resource { // ChatGPT till line 42
+    char *path;
+    char *content;
+    struct Resource *next;
+} Resource;
+
+Resource *resources = NULL;
+
+// Helper: find resource
+Resource *find_resource(const char *path) {
+    for (Resource *r = resources; r != NULL; r = r->next) {
+        if (strcmp(r->path, path) == 0)
+            return r;
+    }
+    return NULL;
+}
+
 /**
 3 *
 2 * Derives a sockaddr_in structure from the provided host and port information.
@@ -43,6 +70,50 @@ static struct sockaddr_in derive_sockaddr(const char *host, const char *port)
     freeaddrinfo(result_info);
     return result;
 }
+/**
+ * Checks if the given HTTP request.
+ * @param request The HTTP request string to be checked.
+ * @return req type, Null if invalid
+ */
+
+int check_http_request(const char *request)
+{
+    // Anfragen sind inkorrekt, wenn
+    // • keine Startzeile erkannt wird, also die erste Zeile nicht dem Muster <Method> <URI> <HTTPVersion>\r\n entspricht oder
+    // • Ihre Header nicht dem Muster <Key>: <Value> entsprechen.
+    char method[16], uri[128], version[16];
+    if (sscanf(request, "%15s %127s %15s\r\n", method, uri, version) != 3)
+        return BADREQUEST; // Bad Request
+
+    char *newline = strchr(request, '\n');
+    if (!newline)
+        return BADREQUEST;
+    char *line_start = newline + 1;
+    while (line_start && *line_start != '\r' && *line_start != '\n')
+    {
+        char key[128], value[256];
+        if (sscanf(line_start, "%127[^:]: %255[^\r\n]\r\n", key, value) != 2)
+        {
+            return BADREQUEST; // Bad Request
+        }
+        char *next = strchr(line_start, '\n');
+        if (!next)
+            break;
+        line_start = next + 1;
+    }
+
+    printf("Method: %s\n", method);
+    if (strcmp(method, "GET") == 0)
+        return GET;
+    else if (strcmp(method, "POST") == 0)
+        return POST;
+    else if (strcmp(method, "PUT") == 0)
+        return PUT;
+    else if (strcmp(method, "DELETE") == 0)
+        return DELETE;
+    else
+        return UNKNOWN; // Unsupported Method
+}
 
 int main(int argc, char *argv[])
 {
@@ -61,16 +132,31 @@ int main(int argc, char *argv[])
     struct sockaddr_storage their_addr; // Copied from Beej's Guide till line
     int sockfd, new_fd;                 // listen on sock_fd, new connection on new_fd
 
-    const char *accept_message = "Reply\r\n\r\n";
+    // const char *accept_message = "Reply\r\n\r\n";
     const char *bad_request_response =
-    "HTTP/1.1 400 Bad Request\r\n"
-    "Content-Length: 0\r\n"
-    "\r\n";
+        "HTTP/1.1 400 Bad Request\r\n"
+        "Content-Length: 0\r\n"
+        "\r\n";
+    const char *not_found_response =
+        "HTTP/1.1 404 Not Found\r\n"
+        "Content-Length: 0\r\n"
+        "\r\n";
+    const char *code_501 =
+        "HTTP/1.1 501 Not Implemented\r\n"
+        "Content-Length: 0\r\n"
+        "\r\n";
+    const char *no_content =
+        "HTTP/1.1 204 No Content\r\n"
+        "Content-Length: 0\r\n"
+        "\r\n";
+    const char *created =
+        "HTTP/1.1 201 Created\r\n"
+        "Content-Length: 0\r\n"
+        "\r\n";
 
-    int bytes_sent;
+
 
     // first, load up address structs with getaddrinfo():
-
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET; // use IPv4
     hints.ai_socktype = SOCK_STREAM;
@@ -83,7 +169,6 @@ int main(int argc, char *argv[])
     }
 
     // make a socket:
-
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (sockfd < 0)
     {
@@ -145,16 +230,94 @@ int main(int argc, char *argv[])
             while ((packet_end = strstr(buf, "\r\n\r\n")) != NULL)
             {
                 // Found one complete HTTP packet
-                if (send(new_fd, bad_request_response, strlen(bad_request_response), 0) < 0)
+                char *packet = strndup(buf, packet_end - buf + 4);
+
+                int reqType = check_http_request(packet);
+                char *response = NULL;
+                char response_buf[512];
+                size_t response_len = 0;
+
+                char method[16], uri[256], version[16];
+                if (sscanf(packet, "%15s %255s %15s", method, uri, version) != 3)
+                {
+                    reqType = BADREQUEST;
+                    break;
+                }
+
+                switch (reqType)
+                {
+                    case GET:
+                    {
+                        /* Valid GET request so inspect URI and serve static content */
+                        const char *body = NULL;
+                        if (strncmp(uri, "/static/", 8) == 0)
+                        {
+                            const char *key = uri + 8;
+                            if (strcmp(key, "foo") == 0)
+                                body = "Foo";
+                            else if (strcmp(key, "bar") == 0)
+                                body = "Bar";
+                            else if (strcmp(key, "baz") == 0)
+                                body = "Baz";
+                        }
+                        //Check dynamic content
+                        if(strncmp(uri, "/dynamic/", 9) == 0)
+                        {
+                            const char *key = uri + 9;
+                            Resource *res = find_resource(key);
+                            if (res)
+                                body = res->content;
+                        }
+
+                        if (body)
+                        {
+                            int len = snprintf(response_buf, sizeof response_buf,
+                                            "HTTP/1.1 200 OK\r\nContent-Length: %zu\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n%s",
+                                            strlen(body), body);
+                            if (len > 0)
+                                response_len = (size_t)len;
+                        }
+                        else
+                        {
+                            response = not_found_response;
+                        }
+                    }
+                    break;
+
+                case PUT:
+                    response = no_content;
+                    break;
+                case DELETE:
+                    response = no_content;
+                    break;
+                case UNKNOWN:
+                    response = code_501;
+                    break;
+                case BADREQUEST:
+                    response = bad_request_response;
+                    break;
+                default:
+                    response = code_501;
+                    break;
+                }
+
+                ssize_t sent = 0;
+                if (response_len > 0) // custom response in buffer
+                {
+                    sent = send(new_fd, response_buf, response_len, 0);
+                }
+                else if (response) // predefined response
+                {
+                    sent = send(new_fd, response, strlen(response), 0);
+                }
+
+                if (sent < 0)
                 {
                     perror("Error sending reply");
                     close(new_fd);
                     break;
                 }
-
-                char *packet = strndup(buf, packet_end - buf + 4);
-
-                printf("Processed a packet and sent reply.\nPacket data:\n%s\n", packet);
+                printf("Processed a packet and sent reply.\nPacket data:\n%s\nResponse:\n%s", packet, response);
 
                 // Move remaining unprocessed data to the start
                 size_t processed_length = packet_end - buf + 4; // include "\r\n\r\n"
@@ -162,6 +325,7 @@ int main(int argc, char *argv[])
                 memmove(buf, buf + processed_length, remaining);
                 buf_len = remaining;
                 buf[buf_len] = '\0';
+                free(packet);
             }
         }
 
